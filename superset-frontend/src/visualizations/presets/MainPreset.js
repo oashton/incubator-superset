914c3d8c8435340190064b5848874d62e4205337
Camilo <camilo.a@uptimeanalytics.com>
2020-09-04 10:43:19 -0500

    Moved pivot table chart to npm uptime org.

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -35,7 +35,7 @@ import MarkupChartPlugin from '@superset-ui/legacy-plugin-chart-markup';
 import PairedTTestChartPlugin from '@superset-ui/legacy-plugin-chart-paired-t-test';
 import ParallelCoordinatesChartPlugin from '@superset-ui/legacy-plugin-chart-parallel-coordinates';
 import PartitionChartPlugin from '@superset-ui/legacy-plugin-chart-partition';
-import PivotTableChartPlugin from '@superset-ui/legacy-plugin-chart-pivot-table';
+import PivotTableChartPlugin from '@superset-ui-uptime/legacy-plugin-chart-pivot-table';
 import RoseChartPlugin from '@superset-ui/legacy-plugin-chart-rose';
 import SankeyChartPlugin from '@superset-ui-uptime/legacy-plugin-chart-sankey';
 import SunburstChartPlugin from '@superset-ui/legacy-plugin-chart-sunburst';
